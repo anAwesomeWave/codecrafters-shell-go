cf2da9d87ae0e676af7ae4738fca3153fbe8735a
Timofei Zubov <anawesomewave@172.31.130.138-vpn.dhcp.yndx.net>
2026-06-21 11:09:09 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
-
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
+
+	var cmd string
+
+	fmt.Scan(&cmd)
+
+	fmt.Printf("%s: command not found", cmd)
 }
